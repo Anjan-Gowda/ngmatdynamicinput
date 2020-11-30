828858f76f6bda69a6df5607fadaa65834602046
Anjan-Gowda <anjan.bc2@gmail.com>
2020-11-30 15:03:41 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule, FormsModule, MatInputModule, BrowserAnimationsModule
+  ],
+  exports: [MatCardModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
